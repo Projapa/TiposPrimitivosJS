9ebf5dcfb37d40cc2a4a29771685eee2781ad55c
Danillo Araki <121911759+Projapa@users.noreply.github.com>
2023-07-03 11:27:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// String
+// Number
+// Boolean
+// null
+// undefined
+
+let nome='Danillo';
+let numero= 1991;
+let usuarioVerificado = true;
+let pessoa = null
+let telefone = undefined
+console.log(telefone)
